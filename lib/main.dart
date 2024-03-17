b770cba1c6e2102e39a88a333b2d42b69bc6a2c5
nuilewis <lewisnuikweh@gmail.com>
2024-03-17 14:51:10 +0100

    setup: change app name

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Sickler',
+        title: 'Zoner',
         debugShowCheckedModeBanner: false,
         theme: ZonerThemeData.lightTheme,
         darkTheme: ZonerThemeData.darkTheme,
